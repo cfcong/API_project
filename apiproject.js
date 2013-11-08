8edde82628fea0dfaeb63cb80d57fd9e8ee8c4f4
Colin Fcong <cfcong@gmail.com>
2013-11-08 00:31:57 +0000

    80% done - took api from city forecast page, still have to choose information to show and execute

--- apiproject.js
+++ apiproject.js
@@ -1,23 +1,29 @@
-$(function(){
+$(function() {
 	//run the query (pull data from feed)
-	var url = 'http://autocomplete.wunderground.com/aq?query=';
+	var dataUrl = 'http://autocomplete.wunderground.com/aq?query=';
 	var searchBar;
+	var cityUrl = 'http://api.wunderground.com/api/55f52aa00e54da0f/forecast';
 	//1)On click of button:
-	$('#searchNow').click(function(){
+	$('#searchNow').click(function() {
 	//a)Get the string typed in by the user (Alert/Console.log it so we can make sure our 
 	//variable has the data)
 		var searchBar = $('#searchBar').val();
 
-		$.getJSON(url + searchBar + '&cb=?',
+		$.getJSON(dataUrl + searchBar + '&cb=?',
 
 			function(json){
-				console.log(json);
-				var results = json["RESULTS"];
-				for(var i = 0; i < results.length; i++){
-					var city = results[i];
-					var li = $('<li></li>');
-					li.append('<div>' + city['name'] + '</div>');
+				//console.log(json);
+				//var results = json["RESULTS"];
+				$.each(json.RESULTS, function (i, outcome) {
+				//for(var i = 0; i < results.length; i++){
+					//var city = results[i];
+					//var li = $('<li></li>');
+					//li.attr('cityInfo', city['l']);
+					//li.append('<div>' + city['name'] + '</div>');
+					var li = $('<li>' + outcome.name + '<li>');
+					li.attr('cityInfo', outcome.l);
 					$('#entrylist').append(li);
+					console.log(outcome);
 					$('#searchBar').val(' ');
 					//$('<li>' + city['name'] + '</li>').click(function(){
 						//val urlDetail = 'http://api.wunderground.com/api/55f52aa00e54da0f/forecast/q/' ;
@@ -27,13 +33,20 @@ $(function(){
 							//}	
 						//)
 					//});
-				}
+				});
 			}
 		)
+		$('ul#entrylist').on('click','li', function() {
+			var cityWeather = $(this).attr('cityInfo');
+			$.getJSON(cityUrl + cityWeather + '.json?cb=?',
 
+				function(json){
+					console.log(json);
+				}
+			)
+		});				
 	});
-});				
-		
+});		
 //0)Examine the JSON response from the Autocomplete API (Paste the URL in a tab in Chrome)
 //On click of button:
 //	-identify how you will grab the data from here
